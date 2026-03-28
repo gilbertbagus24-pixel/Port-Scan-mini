5c18d4564019c052e9f610c4b3b7286149afc1ad
Gilbert Bagus N <gilbertbagus24@gmail.com>
2026-03-28 23:39:14 +0700

    add for ports

--- portscan.py
+++ portscan.py
@@ -2,3 +2,7 @@ import socket
 
 target = input("Enter IP Target: ")
 port = [21, 22, 23, 80, 443]
+
+for port in ports : 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
