d578d1c13bde22308f7afebb3af959f4a89dd82c
Gilbert Bagus N <gilbertbagus24@gmail.com>
2026-03-28 23:39:48 +0700

    Check port status and print if open

--- portscan.py
+++ portscan.py
@@ -6,3 +6,8 @@ port = [21, 22, 23, 80, 443]
 for port in ports : 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
+
+    result = s.connect_ex((target, port))
+    if result == 0:
+        print(f"Port {port} open")
+        s.close()
